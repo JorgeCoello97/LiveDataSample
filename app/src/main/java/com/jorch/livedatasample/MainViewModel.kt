ace7b73acc3828dfe6cfd3d59d8afa49626323fe
Jorge Coello <jorchcoello97@gmail.com>
2021-07-20 23:11:30 +0200

    Simple way to preserver state of viewmodels

--- app/src/main/java/com/jorch/livedatasample/MainViewModel.kt
+++ app/src/main/java/com/jorch/livedatasample/MainViewModel.kt
@@ -2,15 +2,17 @@ package com.jorch.livedatasample
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 
-class MainViewModel : ViewModel() {
+class MainViewModel(private val state: SavedStateHandle) : ViewModel() {
     
-    private val _count = MutableLiveData(0)
+    private val _count = MutableLiveData(state["count"] ?: 0)
     val count: LiveData<Int> get() = _count
 
 
     fun incrementCount() {
         _count.value = _count.value?.plus(1) ?: 1
+        state["count"] = _count.value
     }
 }
